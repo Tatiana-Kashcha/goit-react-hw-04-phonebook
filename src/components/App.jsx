d8e02dfeefb38866a2c8ca2bfb212eb8c2e8370c
Tatiana Kashcha <tkashcha@gmail.com>
2024-12-31 02:13:00 +0200

    read contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,21 @@ import { nanoid } from 'nanoid';
 import ContactForm from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 
+import { db } from '../firebase/firebaseConfig';
+import { collection, addDoc, getDocs, doc } from 'firebase/firestore';
+
 import * as s from './App.styled';
 
-const STORAGE_KEY = 'contact-list';
-
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(STORAGE_KEY)) ?? [];
-  });
+  // const [contacts, setContacts] = useState(() => {
+  //   return JSON.parse(window.localStorage.getItem(STORAGE_KEY)) ?? [];
+  // });
 
-  useEffect(() => {
-    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   window.localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
+
+  const [contacts, setContacts] = useState([]);
 
   const addUser = data => {
     const newUser = {
@@ -22,32 +25,45 @@ const App = () => {
       ...data,
     };
 
-    const isDuplicateUser = contacts.some(
-      contact => contact.name.toLowerCase() === data.name.toLowerCase()
-    );
+    // const isDuplicateUser = contacts.some(
+    //   contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    // );
 
-    if (isDuplicateUser) {
-      alert('This name is already in the contacts list.');
-      return;
-    }
+    // if (isDuplicateUser) {
+    //   alert('This name is already in the contacts list.');
+    //   return;
+    // }
 
-    setContacts(prev => [...prev, newUser]);
+    // setContacts(prev => [...prev, newUser]);
   };
 
+  const getAllContacts = async () => {
+    const querySnapshot = await getDocs(collection(db, 'contacts'));
+    const allContacts = querySnapshot.docs.map(contact => ({
+      id: contact.id,
+      ...contact.data(),
+    }));
+    setContacts(allContacts);
+  };
+
+  useEffect(() => {
+    getAllContacts();
+  }, []);
+
   const deleteUser = id => {
-    setContacts(contacts.filter(el => el.id !== id));
+    // setContacts(contacts.filter(el => el.id !== id));
   };
 
   return (
     <s.Container>
       <h1>Phonebook</h1>
       <ContactForm addUser={addUser} />
-      {contacts.length > 0 && (
-        <>
-          <h2>Contacts</h2>
-          <ContactList data={contacts} deleteUser={deleteUser} />
-        </>
-      )}
+      {/* {contacts.length > 0 && ( */}
+      <>
+        <h2>Contacts</h2>
+        <ContactList data={contacts} deleteUser={deleteUser} />
+      </>
+      {/* )} */}
     </s.Container>
   );
 };
