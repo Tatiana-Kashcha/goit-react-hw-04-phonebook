8bb078c92b7494399b5b7d54d55e15797e1a5cd5
Tatiana Kashcha <tkashcha@gmail.com>
2025-01-31 11:32:42 +0200

    Update Phonebook.styled.js

--- src/components/Phonebook/Phonebook.styled.js
+++ src/components/Phonebook/Phonebook.styled.js
@@ -29,24 +29,3 @@ export const Greeting = styled.p`
     color: rgb(12 80 206);
   }
 `;
-
-// export const Thumb = styled.div`
-//   width: 38px;
-//   height: 38px;
-//   border-radius: 50%;
-//   margin-right: 40px;
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// export const Avatar = styled.img`
-//   border-radius: 50%;
-//   min-width: 38px;
-//   min-height: 38px;
-//   object-fit: cover;
-//   cursor: pointer;
-// `;
-
-// export const Upload = styled.input`
-//   visibility: hidden;
-// `;
