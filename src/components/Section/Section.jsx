a88e0ab79301f5601872ec610316f9ae1584c0c3
Tatiana Kashcha <tkashcha@gmail.com>
2025-02-10 11:20:09 +0200

    upd Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,6 +2,10 @@ import * as s from './Section.styled';
 
 export const Section = ({ title, children }) => (
   <s.Section>
+    <s.Container>
+      <s.StyledLink to="/">{'<<'} HOME</s.StyledLink>
+    </s.Container>
+
     {title && <s.SectionTitle>{title}</s.SectionTitle>}
     {children}
   </s.Section>
--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,7 +1,34 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const Section = styled.section`
   padding: 150px 0;
+`;
+
+export const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 700px;
+`;
+
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+  padding: 10px 0;
+  font-size: 18px;
+  color: rgb(126 79 15);
+  font-weight: bold;
+  border: 1px solid rgb(126 79 15);
+  border-radius: 4px;
+  min-width: 100px;
+  text-align: center;
+  cursor: pointer;
+  background-color: rgb(242 228 178);
+  &:hover {
+    background-color: rgb(126 79 15);
+    color: rgb(242 228 178);
+  }
 `;
 
 export const SectionTitle = styled.h2`
